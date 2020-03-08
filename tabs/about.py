e34331e8df8c8a3c639dc2aef81617cdbad9a1f0
JimKing100 <52686228+JimKing100@users.noreply.github.com>
2020-03-08 11:41:08 -0700

    Update about.py

--- tabs/about.py
+++ tabs/about.py
@@ -7,7 +7,7 @@ from app import app
 layout = [dcc.Markdown("""
 ### About
 
-Marijuana is used for both medical and recreational purposes in the U.S. The two main strain, indica and sativa, have been joined with newer hybrid strains.  Each strain has a unique combination of effects and flavors.  In general, indica strains provide a sense of relaxation and euphoria while sativas provide a sense of energy and alertness.  Hybrids can combine effects of both indica and sativa.
+Marijuana is used for both medical and recreational purposes in the U.S. The two main strains, indica and sativa, have been joined with newer hybrid strains.  Each strain has a unique combination of effects and flavors.  In general, indica strains provide a sense of relaxation and euphoria while sativas provide a sense of energy and alertness.  Hybrids can combine effects of both indica and sativa.
 
 Indica strains are often used for medical purposes because of their ability to reduce inflammation and provide temporary relief from pain.  Examples of popular indica strains are Northern Lights, Bubble Kush, Pineapple Kush and Pineapple Chunk.
 
