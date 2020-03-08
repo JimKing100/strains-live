018c3f88eb898f402249b5d7acea9e524ed93a9f
JimKing100 <52686228+JimKing100@users.noreply.github.com>
2020-03-07 17:17:50 -0800

    Update predict.py

--- tabs/predict.py
+++ tabs/predict.py
@@ -4,7 +4,58 @@ import dash_html_components as html
 
 from app import app
 
-layout = [dcc.Markdown("""
-### Predict
-This is a test
-""")]
+import pandas as pd
+import pickle
+
+from sklearn.neighbors import NearestNeighbors
+
+pickle.load(dtm, open('/model/dtm.pkl', 'rb'))
+pickle.load(tf, open('/model/tf.pkl', 'rb'))
+
+cities = ['Belvedere',
+          'Bolinas',
+          'Corte Madera',
+          'Dillon Beach',
+          'Fairfax',
+          'Greenbrae',
+          'Inverness',
+          'Kentfield',
+          'Larkspur',
+          'Marshall',
+          'Mill Valley',
+          'Muir Beach',
+          'Nicasio',
+          'Novato',
+          'Point Reyes',
+          'Ross',
+          'San Anselmo',
+          'San Geronimo Valley',
+          'San Rafael',
+          'Sausalito',
+          'Stinson Beach',
+          'Tiburon',
+          'Tomales',
+          'Other'
+          ]
+
+style = {'padding': '1.5em'}
+
+layout = html.Div([
+    dcc.Markdown("""
+        ### Predict
+        Use the controls below to update your predicted offer, based on city,
+        beds, baths, number of offers, and list price.
+    """),
+
+    html.Div(id='prediction-content', style={'fontWeight': 'bold'}),
+
+    html.Div([
+        dcc.Markdown('###### Area'),
+        dcc.Dropdown(
+            id='area',
+            options=[{'label': city, 'value': city} for city in cities],
+            value=cities[10]
+        ),
+    ], style=style),
+  
+])
