821c4054c503973498211be8f0d687ec074504cc
JimKing100 <52686228+JimKing100@users.noreply.github.com>
2020-03-08 11:08:37 -0700

    Update intro.py

--- tabs/intro.py
+++ tabs/intro.py
@@ -7,7 +7,11 @@ from app import app
 layout = [dcc.Markdown("""
 ### Introduction
 
-This web app enables users to enter various combinations of effects and flavors for over 2,300 marijuana strains and returns five recommended stains meeting the users criteria.
+This web app enables users to enter various combinations of effects and flavors for over 2,300 marijuana strains and returns five recommended stains meeting the users criteria.  Click on the Recommend tab to obtain recommendations.
+
+#### Methodology
+
+The effects and features for the 2,300+ marijuana strains are tokenized and vectorized using Term Frequency-Inverse Document Frequency (TF-IDF) to create a document-term matrix (dtm).  The dtm is then fit on a Nearest Neighbors model.  The model is then used to obtain the 5 closest matching strains based on the users selections of 5 effects and 3 flavors (8 features).
 
 #### Dataset
 
