88ac77660e30e93eff295a20b01730bf67ffa344
JimKing100 <52686228+JimKing100@users.noreply.github.com>
2020-03-07 16:56:07 -0800

    Create explain.py

---
+++ tabs/explain.py
@@ -0,0 +1,10 @@
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+
+from app import app
+
+layout = [dcc.Markdown("""
+### Explain
+This is a test
+""")]
